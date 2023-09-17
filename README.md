f9a995c09e9e4929459d843d333a34a52ca2e070
RyanNgCT <48358569+RyanNgCT@users.noreply.github.com>
2023-09-17 17:08:52 +0800

    v0.1 - Bare Bones

---
+++ README.md
@@ -0,0 +1,9 @@
+# SecureQRParse-CLI
+
+- Python Script to extract urls and defang them from QR Codes.
+- Command-Line Tool Interface _for now_ (can use `--file` or `--directory` to extract urls from a given file/directory).
+
+## Possible To-dos
+
+- allow for recursive search (depth > 1) when looking at files in subdirectories/subfolders, while using the `--directory` flag.
+- convert and host python script as an interactive Flask Web-App.
---
+++ parse.py
@@ -0,0 +1,88 @@
+import PIL
+from PIL import Image
+from pyzbar.pyzbar import decode
+from pathlib import Path
+import argparse, sys, re
+
+
+def parseSingleQR(file : Path) -> tuple[str, bool]:
+    """ based on a file, determine if whether the file contains a valid QR Code 
+        and return the corresponding URL, if any, defanged. """
+    
+    try:
+        fileExif = decode(Image.open(file))
+    except PIL.UnidentifiedImageError:
+        return "File supplied is not a valid image or QR Code.", False
+    else:
+        # check type of image and if data field exists
+        if fileExif[0].type == "QRCODE" and fileExif[0].data:
+            rawData = fileExif[0].data.decode()
+            uri_pattern = r"^(?:https?|hxxps?):\/\/(?:\[\.\]|\[\.\]\[\.\]|[^\[\]])+|(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})?)(?:$|\s)"
+            if re.match(uri_pattern, rawData):
+                return defangURL(rawData), True
+            else:
+                return "No url detected in QR Code.", False
+        return "File is not/does not contain a QR Code.", False
+
+
+def parseQRsInDir(dirPath : Path) -> list[str]:
+    """ based on a directory, enumerate over its file contents and run 
+        parseSingleQR() => to make recursive """
+
+    allExtractedUrls = {} # maybe for future use
+    extractedUrls = [] # list of defanged urls to be returned
+    validUrls = 0  # counter for valid URLs
+    index = 1
+    totalFiles = len(list(dirPath.rglob("*"))) # total number of files for target directory
+    for file in dirPath.rglob("*"):
+        if file is not None:
+            result, isValidUrl = parseSingleQR(file)
+            allExtractedUrls[index] = result
+            if isValidUrl:
+                validUrls += 1
+                extractedUrls.append(result)
+            index += 1
+
+    print(f"[INFO]: Decoded {validUrls} of {totalFiles} files at location: '{dirPath.resolve()}'")
+
+    return list(set(extractedUrls)) # remove duplicate urls, if any
+
+
+def defangURL(rawUrl : str) -> str:
+    return rawUrl.replace(".", "[.]").replace("http", "hxxp")
+
+
+def main() -> None:
+    argDesc = '''SecureQRParse v0.1, (c) RyanNgCT, 2023'''
+    parser = argparse.ArgumentParser(formatter_class = argparse.RawDescriptionHelpFormatter, description=argDesc)
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-f", "--file", help="File to be parsed.")
+    group.add_argument("-d", "--directory", help="Directory of file(s) to be parsed")
+    args = parser.parse_args()
+
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+
+    if args.file:
+        pathOfFile = Path(args.file)
+        if pathOfFile.is_file():
+            retStr, isValidUrl = parseSingleQR(pathOfFile)
+        else:
+            sys.exit("Specified location is not a file / does not exist.")
+
+    elif args.directory:
+        pathOfDir = Path(args.directory)
+        if pathOfDir.is_dir():
+            retStr = parseQRsInDir(pathOfDir)
+        else:
+            sys.exit("Specified location is not a directory / does not exist.")
+    
+    if args.directory or isValidUrl:
+        print("Url(s): ", retStr)
+    else:
+        print(retStr)
+
+
+if __name__ == "__main__":
+    main()
